b09524822aa66ad52c61c63bd525a384e5899d5e
bugltd <194549623+bugltd@users.noreply.github.com>
2025-01-14 16:02:34 +0100

    remove experimental nodes

--- src/ts/register.ts
+++ src/ts/register.ts
@@ -9,13 +9,6 @@ import { setupDynamicConnections } from '~/shared/dynamic_connections.js'
 import { QUEUE_STATUS } from '~/widgets/queue.js'
 import { HIDDEN, BTN, ERROR_DISPLAY } from '~/widgets/misc.js'
 import { OutputConfigBackend, OutputConfigLocal } from '~/nodes/config.js'
-import {
-	NodeConfigFetch,
-	NodeConfigApplyLocal,
-	MultiConfigFetch,
-	MultiConfigApplyLocal,
-} from '~/nodes/experimental/config.node_multi.js'
-import { OutputConfigAuto } from '~/nodes/experimental/config.output.auto.js'
 import { ListFromMultiline } from '~/nodes/list.js'
 import { InputMultiline } from '~/nodes/input.js'
 import { DebugJSONYAML, DebugWidgetVisibility } from '~/nodes/debug.js'
@@ -77,21 +70,6 @@ app.registerExtension({
 				break
 			case 'OutputConfigLocal':
 				OutputConfigLocal(nodeType)
-				break
-			case 'OutputConfigAuto':
-				OutputConfigAuto(nodeType)
-				break
-			case 'NodeConfigFetch':
-				NodeConfigFetch(nodeType)
-				break
-			case 'NodeConfigApplyLocal':
-				NodeConfigApplyLocal(nodeType)
-				break
-			case 'MultiConfigFetch':
-				MultiConfigFetch(nodeType)
-				break
-			case 'MultiConfigApplyLocal':
-				MultiConfigApplyLocal(nodeType)
 				break
 		}
 
--- src/ts/shared/config.common.ts
+++ src/ts/shared/config.common.ts
@@ -1,13 +1,5 @@
 import { fetchApi } from './utils.js'
 import { IOutputParsed, IOutputErrors } from './config.output.js'
-import {
-	INodeConfigParsed,
-	INodeConfigErrors,
-} from '~/shared/experimental/config.node.js'
-import {
-	IMultiConfigParsed,
-	IMultiConfigErrors,
-} from '~/shared/experimental/config.multi.js'
 
 export async function parseConfig(raw: string) {
 	return await fetchApi('config_parse', 'POST', { raw })
@@ -17,8 +9,8 @@ export type ApiValidationError = [string[], string]
 export type WidgetValue = string | number | boolean
 
 export type IConfigValidationResult<
-	TData extends IOutputParsed | INodeConfigParsed | IMultiConfigParsed,
-	TErrors extends IOutputErrors | INodeConfigErrors | IMultiConfigErrors,
+	TData extends IOutputParsed,
+	TErrors extends IOutputErrors,
 > =
 	| {
 			success: true
